e574ed022e2d4c6139acefad9611ad82931a1e72
sxhyxnchxng <sxhyxnchxng@gmail.com>
2018-10-03 15:08:01 +0900

    firebase 설치

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase';
 
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+const config = {
+  apiKey: 'AIzaSyCVs37OGEDlUKV7HjNjafLWVExLcu_gG0M',
+  authDomain: 'nomusic-web.firebaseapp.com',
+  databaseURL: 'https://nomusic-web.firebaseio.com',
+  projectId: 'nomusic-web',
+  storageBucket: '',
+  messagingSenderId: '834312253465',
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
